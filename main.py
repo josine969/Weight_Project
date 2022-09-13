cc5d32bbed55f2e6cc11fd2e80e7e527915cd3c2
josine969 <jlusipa@hotmail.com>
2022-09-13 18:43:50 +0200

    main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+today = date.today()
+print("Today's date:", today)
+
+# Ask for the data
+
+#print("¿?")
+#nombre = input()
+#print(f"Me alegro de conocerle, {nombre}")
+
+# Add value
+#curs.execute("insert into run values ('05/06', 34.20, 'Not bad')")
